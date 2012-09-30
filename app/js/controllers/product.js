04e949509a020a5f0499f55fc5668d70064d40c3
Kaique da Silva <kaique.developer@gmail.com>
2012-09-30 21:10:05 +0000

    product search

--- app/js/controllers/product.js
+++ app/js/controllers/product.js
@@ -5,27 +5,27 @@ var ProductListControl = function ProductListControl ( $scope ) {
         price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }, {
-        name: 'Product one',
+        name: 'Product two',
         thumb: 'http://www.placehold.it/240x240',
         price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }, {
-        name: 'Product one',
+        name: 'Product three',
         thumb: 'http://www.placehold.it/240x240',
         price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }, {
-        name: 'Product one',
+        name: 'Product four',
         thumb: 'http://www.placehold.it/240x240',
         price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }, {
-        name: 'Product one',
+        name: 'Product five',
         thumb: 'http://www.placehold.it/240x240',
         price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }, {
-        name: 'Product one',
+        name: 'Product six',
         thumb: 'http://www.placehold.it/240x240',
         price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
