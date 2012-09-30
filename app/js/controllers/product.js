aa054caf7a049f11456462ebdb0ad2d21792587f
Kaique da Silva <kaique.developer@gmail.com>
2012-09-30 19:55:19 +0000

    listing products

---
+++ app/js/controllers/product.js
@@ -0,0 +1,11 @@
+var ProductListControl = function ProductListControl ( $scope ) {
+    $scope.products = [{
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/150x150',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/150x150',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }];
+};
