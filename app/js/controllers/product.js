9387f586713f180b0c8380981488ae2ae9a0fb5e
Kaique da Silva <kaique.developer@gmail.com>
2012-09-30 20:23:13 +0000

    style ok

--- app/js/controllers/product.js
+++ app/js/controllers/product.js
@@ -1,11 +1,33 @@
 var ProductListControl = function ProductListControl ( $scope ) {
     $scope.products = [{
         name: 'Product one',
-        thumb: 'http://www.placehold.it/150x150',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }, {
         name: 'Product one',
-        thumb: 'http://www.placehold.it/150x150',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
+    }, {
+        name: 'Product one',
+        thumb: 'http://www.placehold.it/240x240',
+        price: 'R$ 10.00',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et semper neque. Mauris cursus porta neque ut dapibus. Nunc porta magna metus.'
     }];
 };
